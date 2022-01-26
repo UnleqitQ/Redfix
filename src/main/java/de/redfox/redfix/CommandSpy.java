2ff794fa82ccd4c4835e5480c3cb8cac76f7d201
UnleqitQ <quentin.weinbrenner@gmail.com>
2022-01-26 21:23:39 +0100

    Update

--- src/main/java/de/redfox/redfix/CommandSpy.java
+++ src/main/java/de/redfox/redfix/CommandSpy.java
@@ -2,6 +2,9 @@ package de.redfox.redfix;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,7 +15,7 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
-public class CommandSpy implements Listener {
+public class CommandSpy implements Listener, CommandExecutor {
 	
 	public Set<UUID> players = new HashSet<>();
 	
@@ -20,16 +23,32 @@ public class CommandSpy implements Listener {
 		Bukkit.getPluginManager().registerEvents(this, RedfixPlugin.getInstance());
 	}
 	
+	@Override
+	public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+		if (commandSender instanceof Player) {
+			Player player = (Player) commandSender;
+			if (players.contains(player.getUniqueId())) {
+				players.remove(player.getUniqueId());
+				player.sendMessage("Disabled CommandSpy");
+			}
+			else {
+				players.add(player.getUniqueId());
+				player.sendMessage("Enabled CommandSpy");
+			}
+		}
+		return true;
+	}
+	
 	@EventHandler
-	public void onCommand(@NotNull PlayerCommandSendEvent event) {
+	public void onCommandSent(@NotNull PlayerCommandSendEvent event) {
 		String[] msgs = new String[event.getCommands().size()];
 		int i = 0;
 		for (String cmd : event.getCommands()) {
 			msgs[i++] = ChatColor.GRAY + "[CommandSpy] " + ChatColor.GREEN + event.getPlayer().getName() + ChatColor.WHITE + ": " + cmd;
 		}
 		for (UUID uuid : players) {
-			if (event.getPlayer().getUniqueId().equals(uuid))
-				continue;
+			//if (event.getPlayer().getUniqueId().equals(uuid))
+			//	continue;
 			Player player = Bukkit.getPlayer(uuid);
 			if (player != null) {
 				for (String msg : msgs) {
--- src/main/java/de/redfox/redfix/RedfixPlugin.java
+++ src/main/java/de/redfox/redfix/RedfixPlugin.java
@@ -1,13 +1,28 @@
 package de.redfox.redfix;
 
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class RedfixPlugin extends JavaPlugin {
+import java.util.Objects;
 
+public class RedfixPlugin extends JavaPlugin {
+	
 	private static RedfixPlugin instance;
+	public CommandSpy commandSpy;
 	
 	public RedfixPlugin() {
 		instance = this;
+		
+	}
+	
+	@Override
+	public void onEnable() {
+		commandSpy = new CommandSpy();
+		registerCommand("commandspy", commandSpy);
+	}
+	
+	public <T extends CommandExecutor> void registerCommand(String cmd, T handler) {
+		Objects.requireNonNull(getCommand(cmd)).setExecutor(handler);
 	}
 	
 	public static RedfixPlugin getInstance() {
