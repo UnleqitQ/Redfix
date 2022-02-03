bea386c43e66a9ba255565625f826ecb21341e0a
BuildTools <unconfigured@null.spigotmc.org>
2022-02-03 21:14:03 +0100

    Update

--- src/main/java/de/redfox/redfix/RedfixPlugin.java
+++ src/main/java/de/redfox/redfix/RedfixPlugin.java
@@ -1,13 +1,21 @@
 package de.redfox.redfix;
 
+import cloud.commandframework.Command;
+import cloud.commandframework.CommandTree;
+import cloud.commandframework.bukkit.parsers.PlayerArgument;
+import cloud.commandframework.execution.AsynchronousCommandExecutionCoordinator;
+import cloud.commandframework.execution.CommandExecutionCoordinator;
+import cloud.commandframework.paper.PaperCommandManager;
 import de.redfox.redfix.commands.CommandSpy;
 import de.redfox.redfix.config.ConfigManager;
-import de.redfox.redfix.config.LinkedPath;
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
 import java.util.Objects;
+import java.util.function.Function;
 
 public class RedfixPlugin extends JavaPlugin {
 	
@@ -19,13 +27,43 @@ public class RedfixPlugin extends JavaPlugin {
 		instance = this;
 		new File(pluginPath).mkdirs();
 	}
-	
+
+
+
+	private PaperCommandManager<CommandSender> manager;
+	Function<CommandTree<CommandSender>, CommandExecutionCoordinator<CommandSender>> executionCoordinatorFunction =
+					AsynchronousCommandExecutionCoordinator.<CommandSender>newBuilder().build();
+	Function<CommandSender, CommandSender> mapperFunction = Function.identity();
+
 	@Override
 	public void onEnable() {
 		ConfigManager.init();
 		commandSpy = new CommandSpy();
 		commandSpy.load();
 		registerCommand("commandspy", commandSpy);
+		registerCommands();
+	}
+
+	private void registerCommands() {
+		try {
+			manager = new PaperCommandManager<>(this, executionCoordinatorFunction, mapperFunction, mapperFunction);
+			this.getLogger().severe("Failed to initialize the command this.manager");
+			this.getServer().getPluginManager().disablePlugin(this);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		Command.Builder<CommandSender> builder = this.manager.commandBuilder("builder");
+		this.manager.command(builder.literal("jail")
+				.senderType(Player.class)
+				.argument(PlayerArgument.of("player"))
+				.handler(commandContext -> {
+					CommandSender sender = commandContext.getSender();
+					sender.sendMessage("You jailed " + commandContext.getRawInput().get(0));
+					Player player = commandContext.get("player");
+					player.sendMessage("Jailed XD");
+				})
+		);
 	}
 	
 	public void registerCommand(String cmd, CommandExecutor handler) {
