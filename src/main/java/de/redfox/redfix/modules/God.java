75cd4aba101fb388db1bc5cdd0a6cec6c9145ea1
UnleqitQ <quentin.weinbrenner@gmail.com>
2022-02-03 22:30:40 +0100

    Update

---
+++ src/main/java/de/redfox/redfix/modules/God.java
@@ -0,0 +1,61 @@
+package de.redfox.redfix.modules;
+
+import de.redfox.redfix.RedfixPlugin;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.*;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+public class God implements Listener {
+	
+	public static Set<UUID> players = new HashSet<>();
+	
+	public God() {
+		Bukkit.getPluginManager().registerEvents(this, RedfixPlugin.getInstance());
+	}
+	
+	@EventHandler
+	public void onDamage(@NotNull EntityDamageEvent event) {
+		if (event.getEntityType() == EntityType.PLAYER)
+			if (players.contains(event.getEntity().getUniqueId()))
+				event.setCancelled(true);
+	}
+	
+	@EventHandler
+	public void onDamage(@NotNull EntityDamageByEntityEvent event) {
+		if (event.getEntityType() == EntityType.PLAYER)
+			if (players.contains(event.getEntity().getUniqueId()))
+				event.setCancelled(true);
+	}
+	
+	@EventHandler
+	public void onDamage(@NotNull EntityDamageByBlockEvent event) {
+		if (event.getEntityType() == EntityType.PLAYER)
+			if (players.contains(event.getEntity().getUniqueId()))
+				event.setCancelled(true);
+	}
+	
+	@EventHandler
+	public void onLooseAir(@NotNull EntityAirChangeEvent event) {
+		if (event.getEntityType() == EntityType.PLAYER)
+			if (players.contains(event.getEntity().getUniqueId())) {
+				//event.setCancelled(true);
+				event.setAmount(20);
+			}
+	}
+	
+	@EventHandler
+	public void onExhaustion(@NotNull EntityExhaustionEvent event) {
+		if (event.getEntityType() == EntityType.PLAYER)
+			if (players.contains(event.getEntity().getUniqueId())) {
+				event.setCancelled(true);
+			}
+	}
+	
+}
